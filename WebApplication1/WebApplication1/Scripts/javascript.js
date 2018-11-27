e4248f8272b3384f479fc7f36760d285a0aa84e4
Mahesh Kotekar <mvkotekar@gmail.com>
2018-11-27 22:51:25 +0530

    first commit

---
+++ WebApplication1/WebApplication1/Scripts/javascript.js
@@ -0,0 +1,5 @@
+﻿function getDetails() {
+     $get('dataValue').value = $get('MainContent_txtToken').value;
+    $get('txtToken').value = $get('MainContent_txtToken').value;
+    alert('kotekar');
+}
